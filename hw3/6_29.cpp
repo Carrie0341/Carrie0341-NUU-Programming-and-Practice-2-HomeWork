0c56303ecd470b9742c82f0378a36ce232913002
Carrie0341 <64569172+Carrie0341@users.noreply.github.com>
2021-04-21 00:26:58 +0800

    hw3

---
+++ hw3/6_29.cpp
@@ -0,0 +1,42 @@
+/*
+ * 6_29.cpp
+ *
+ *  Created on: 2021年4月15日
+ *      Author: U0924015羅子程
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int count1{0}, count2{0};
+
+bool IsPrime(int n){ // Check if n is prime
+    for (int i = 2; i <= n / 2 && n != 2; i++){
+        count1++;
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+bool IsPrime_fast(int n){ // Check if n is prime
+    for (int i = 2; i <= sqrt(n) && n != 2; i++){
+        count2++;
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main(int argc, char const *argv[]){
+    for (int i = 2; i <= 10000; i++) // Print primes use IsPrime()
+        if (IsPrime(i))
+            cout << i << " ";
+    cout << "\n***** the upper limit of n/2 \n executing times: " << count1 << "\n"<<endl;
+
+    for (int i = 2; i <= 10000; i++) // Print primes use IsPrime_fast()
+        if (IsPrime_fast(i))
+            cout << i << " ";
+    cout << "\n***** the upper limit of sqrt(n) \n executing times: " << count2 << endl;
+    return 0;
+}
---
+++ hw3/6_30.cpp
@@ -0,0 +1,28 @@
+/*
+ * 6_30.cpp
+ *
+ *  Created on: 2021年4月20日
+ *      Author: U0924015羅子程
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+unsigned long int Reverse(unsigned long int _input){ // reverse the input
+    unsigned long int result{0};
+    while (_input > 0){
+        result = result * 10 + _input % 10;
+        _input /= 10;
+    }
+    return result;
+}
+
+int main(int argc, char const *argv[]){
+
+    unsigned long int input{0};
+    cout << "Enter a number: " && cin >> input ; // input
+    cout << "Reverse is: " << Reverse(input)<<endl; // ouput
+
+    return 0;
+}
